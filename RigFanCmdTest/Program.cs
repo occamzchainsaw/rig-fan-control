718e94f1b882244b87972cf862c05152f1240c6d
occamzchainsaw <achtelik.p@gmail.com>
2026-07-04 14:33:43 +0200

    initial commit

---
+++ RigFanCmdTest/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace RigFanCmdTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ RigFanControl.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace RigFanControl.Core;
+
+public class Class1
+{
+
+}
